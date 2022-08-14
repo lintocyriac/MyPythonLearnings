d88c14f344fa06266fcebb0716bcb23ea21a9a24
linto-intelia <linto.cyriac@intelia.com.au>
2022-08-15 06:42:09 +1000

    Final Commit

--- LeapYear.py
+++ LeapYear.py
@@ -1,3 +1,27 @@
 # Accept Input
+def input_year():
+    print("Enter the Input :")
+    a = int(input())
+    print(f'Checking if {a} is a Leap Year...')
+    return a
+
 # Check Whether the input is a Leap year
-# Display the result
+def leap_year(a):
+    if(a%400 == 0):
+        print(f'{a} is a Leap Year as it is divisible by 400.')
+        print('Process finished with Exit Code 0.')
+    elif(a%100 == 0):
+        print(f'{a} is not a Leap Year as it is divisible by 100 but not by 400.')
+        print('Process finished with Exit Code 0.')
+    elif(a%4 == 0):
+        print(f'{a} is a Leap Year as it is divisible by 4 but not by 100 or 400.')
+        print('Process finished with Exit Code 0.')
+    else:
+        print(f'{a} is not a Leap Year as it is not divisible by 4.')
+        print('Process finished with Exit Code 0.')
+
+    
+a= input_year()
+leap_year(a)
+
+
